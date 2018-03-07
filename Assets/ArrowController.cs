a95bd88576c57918319bd7e37d29aa79d85e602f
kazu0419 <sumalove.1214@gmail.com>
2018-03-07 13:56:00 +0900

    プレイヤーと矢の当たり判定を追加

--- Assets/ArrowController.cs
+++ Assets/ArrowController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour {
 
+	GameObject player;
+
 	// Use this for initialization
 	void Start () {
+		this.player = GameObject.Find("player"); 
 		
 	}
 	
@@ -17,6 +20,18 @@ public class ArrowController : MonoBehaviour {
 		// 画面外に出たら、オブジェクトを破壊する
 		if(transform.position.y < -5.0f){
 			Destroy(gameObject);
+		}
+
+		// 当たり判定追加
+		Vector2 p1 = transform.position;              // 矢の中心座標
+		Vector2 p2 = this.player.transform.position;  // プレイヤーの中心座標
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude;
+		float r1 = 0.5f; // 矢の半径
+		float r2 = 1.0f; // プレイヤーの半径
+
+		if(d < r1 + r2){
+			Destroy(gameObject); //衝突した場合は、矢を消す
 		}
 	}
 }
