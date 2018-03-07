e3bd04085cf331a40ccc8dfc5fa89863d8bd8f11
kazu0419 <sumalove.1214@gmail.com>
2018-03-07 13:25:40 +0900

    オブジェクトの配置+プレイヤーと矢の動作スクリプトを追加

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// フレームごとに等速で落下させる
+		transform.Translate(0, -0.1f, 0);
+		
+		// 画面外に出たら、オブジェクトを破壊する
+		if(transform.position.y < -5.0f){
+			Destroy(gameObject);
+		}
+	}
+}
